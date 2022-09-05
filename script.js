303778d413ff10ae8feb04219744f4660f374bca
AhmadEssaGit <killer23mc23@gmail.com>
2022-09-05 17:12:34 +0200

    Update styles to be responsive

---
+++ script.js
@@ -0,0 +1,10 @@
+const main = document.querySelector('.main'); 
+const container = document.querySelector('.container') ; 
+const left = document.querySelector('.left'); 
+
+
+function dig() {
+	container.style.display = 'flex' ; 
+	container.style.align_content = 'center';
+}
+
